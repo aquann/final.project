86be8c9a1aa2bb7d19b3c0a687cb6e50fc67368b
vukanmrvaljevic <31517206+vukanmrvaljevic@users.noreply.github.com>
2018-05-22 15:44:09 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -13,49 +13,67 @@ var GIFset1 = [
     ["missionimpossible", "dogs", "apples", "octopus"],
     ["trump", "sky", "michaelmyers", "hats"]
 ];
+
+var submit = ["winning","losing"];
+
+var score = 0;
+
+var answers = ["007", "pinkpanther", "pokemon", "godfather", "indianajones", "starwars", "freshprince", "lionking", "missionimpossible", "michaelmyers"];
+
 var api = "https://" + host + path + "?api_key=" + apiKey + "&tag=";
-for (let i = 0; i < GIFset1.length; i++) {
-    const gifArray = GIFset1[i];
-    for (let j = 0; j < gifArray.length; j++)  {
+
+var count = 0;
+
+function gifs() {
+    const gifArray = GIFset1[count];
+    for (let j = 0; j < gifArray.length; j++) {
         const gifTag = gifArray[j];
-       $.getJSON(api + gifTag, function(json) {
-           console.log(json.data.image_url);
-           if (json.data.length > 0) 
+        $.getJSON(api + gifTag, function (json) {
+            console.log(json.data.image_url);
+            if (json.data.length > 0)
                 console.log(json);
-           var elem = document.getElementById('q'+i);
-           console.log(elem);
-       });
+            var elem = document.getElementById('q' + count);
+            var img = new Image();
+            img.onclick = function () {
+               img.style.borderStyle="dotted";
+                if (gifTag == answers[count]) {
+                    score++;
+                } else {
+                    score--;
+                }
+            }
+            console.log(score);
+            img.src = json.data.image_url;
+            img.width = "100";
+            elem.appendChild(img);
+        });
     }
 }
+
+gifs();
+var questions = document.getElementsByClassName("question");
+console.log(questions);
+next.onclick = function () {
+    console.log(questions[count])
+    questions[count].style.display = "none";
+    count++;
+    questions[count].style.display = "block";
+    gifs();
+}
+
+var HintButtons = document.getElementsByClassName("HintButton")
 for (let i = 0; i < HintButtons.length; i++) {
-	const img = json.data[i];
-	const imgElem = $('&lt;img&gt;')
-		.attr('src', img.images.downsized.url);
-	$('body').append(imgElem);
-}
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    var b = HintButtons[i];
+    b.onclick = function () {
+        this.previousElementSibling.style.display = "block"
     }
+
+}
+
+fin.onclick = function(){
+    console.log(score);
+    g = document.createElement('div');
+    g.setAttribute("id", "Div1").innerText = "Result is " + score;
+    
+    
 }
